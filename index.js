b9472b46bb5b1fb4c7a9f57cd6422ba769a36462
Kevin Mclaren Pandoh <kevinmpandoh@gmail.com>
2024-09-30 22:16:06 +0800

    Mengubah file index.js

--- index.js
+++ index.js
@@ -9,12 +9,35 @@ form.addEventListener("submit", function (e) {
 
   let footer = document.querySelector("footer");
   let header = document.querySelector("header");
-  let section = document.querySelector("section");
+  let h2 = document.querySelectorAll("h2");
+  let root = document.documentElement;
 
   //   Ubah warna
   if (value === "HTML") {
-    header.style.backgroundColor = "red";
-    footer.style.backgroundColor = "red";
-    section.style.color = "red";
+    // header.style.backgroundColor = "red";
+    // footer.style.backgroundColor = "red";
+    // for (let i = 0; i < h2.length; i++) {
+    //   h2[i].style.color = "red";
+    // }
+    root.style.setProperty("--text-color", "pink");
+    root.style.setProperty("--primary-color", "black");
+  } else if (value === "CSS") {
+    header.style.backgroundColor = "blue";
+    footer.style.backgroundColor = "blue";
+    for (let i = 0; i < h2.length; i++) {
+      h2[i].style.color = "blue";
+    }
+  } else if (value === "Javascript") {
+    header.style.backgroundColor = "yellow";
+    footer.style.backgroundColor = "yellow";
+    for (let i = 0; i < h2.length; i++) {
+      h2[i].style.color = "yellow";
+    }
+  } else {
+    header.style.backgroundColor = "green";
+    footer.style.backgroundColor = "green";
+    for (let i = 0; i < h2.length; i++) {
+      h2[i].style.color = "green";
+    }
   }
 });
